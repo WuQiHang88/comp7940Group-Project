61619cde4f87077e5415c292a70086d25dc079f4
WuQiHang88 <wuqihang2002@gmail.com>
2026-04-12 21:16:38 +0800

    new features

--- DBUtil.py
+++ DBUtil.py
@@ -1,17 +1,36 @@
 import pymongo
+from datetime import datetime  
 
-def save_chat_history(config, user_id, user_msg, bot_reply):
+def save_chat_history(config, user_id, user_message, bot_response):
     try:
         client = pymongo.MongoClient(config['MONGODB']['URI'])
         db = client[config['MONGODB']['DB_NAME']]
-        coll = db['chat_history']
+        collection = db['chat_history']
 
-        coll.insert_one({
+        record = {
             "user_id": user_id,
-            "user_message": user_msg,
-            "bot_response": bot_reply
-        })
+            "user_message": user_message,
+            "bot_response": bot_response,
+            "timestamp": datetime.now()  
+        }
+        collection.insert_one(record)
 
-        client.close()
     except Exception as e:
-        print("DB Error:", e)
+        print("DB Save Error:", e)
+
+def get_recent_chat_history(config, user_id, limit=3):
+    try:
+        client = pymongo.MongoClient(config['MONGODB']['URI'])
+        db = client[config['MONGODB']['DB_NAME']]
+        collection = db['chat_history']
+
+        records = list(collection.find(
+            {"user_id": user_id}
+        ).sort("timestamp", pymongo.DESCENDING).limit(limit))
+
+        records.reverse()
+        return records
+
+    except Exception as e:
+        print("DB Query Error:", e)
+        return []
